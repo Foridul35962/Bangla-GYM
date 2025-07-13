d350881e35af8918e48360134b8bdc66cfd92538
Foridul35962 <foridulislam35962@gmail.com>
2025-07-13 06:27:52 +0600

    link menu section done

--- JS/app.js
+++ JS/app.js
@@ -7,11 +7,24 @@ toggle.addEventListener("click",()=>{
     navLink.classList.toggle("active");
 });
 
-//close click time
+//close menu bar by click time
 allNavLink.forEach((link)=>{
     link.addEventListener("click",()=>{
         
         toggle.classList.remove("active");
         navLink.classList.remove("active");
+    });
+});
+
+// link header to section
+const section = document.querySelectorAll('a[href^="#"]');
+section.forEach(anchor=>{
+    anchor.addEventListener("click",function(e){
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'))
+        if(targetElement){
+            targetElement.scrollIntoView({
+            behavior: 'smooth'
+        })}
     });
 });
